80d982644388a0c97fe80fdc5f8e0021c79b32df
Viktor <noonehasacat@gmail.com>
2024-04-09 18:08:00 +0300

    Fixed imports; Field type; now it runs

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-from contextlib import asynccontextmanager
+import uvicorn
 
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis import Redis
+
 from core.config import redis_config
 from db import redis
 from db.postgres import create_database
@@ -27,3 +29,10 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     debug=True,
 )
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8000,
+    )
