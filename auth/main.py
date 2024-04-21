ebab5ed4622f7fb47420b89500340baef714c3f9
Aidar Garikhanov <a1d4r@yandex.ru>
2024-04-21 21:31:45 +0300

    Endpoints to manage user roles

--- auth/main.py
+++ auth/main.py
@@ -8,6 +8,7 @@ from fastapi.responses import ORJSONResponse
 
 from auth.api.v1.roles import router as roles_router
 from auth.api.v1.user import router as user_router
+from auth.api.v1.users import router as users_router
 from auth.db.fixtures import create_roles
 from auth.db.postgres import create_database
 from auth.db.redis import redis
@@ -28,7 +29,7 @@ async def lifespan(_app: FastAPI) -> AsyncIterator[None]:
 
 
 app = FastAPI(
-    title="Change_to_var",  # TODO: change to variable
+    title="Auth API",
     docs_url="/api/docs",
     lifespan=lifespan,
     openapi_url="/api/openapi.json",
@@ -37,6 +38,7 @@ app = FastAPI(
 )
 app.include_router(roles_router, prefix="/api/v1/roles", tags=["Роли"])
 app.include_router(user_router, prefix="/api/v1/user", tags=["Пользователь"])
+app.include_router(users_router, prefix="/api/v1/users", tags=["Пользователи"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)  # noqa: S104
