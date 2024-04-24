8680f3d01e8dcf7f7ac36908c7862b2bd8eaff6b
OrinaryWorker <sm0ke95@mail.ru>
2024-04-24 09:03:23 +0300

    Update role creation

--- auth/schemas/permission.py
+++ auth/schemas/permission.py
@@ -2,8 +2,14 @@ from pydantic import BaseModel
 from uuid import UUID
 
 
+class PermissionId(BaseModel):
+    """Схема для передачи id ограничения в объект роли."""
+
+    id: str
+
+
 class PermissionCreate(BaseModel):
-    """Схема для создания новой роли."""
+    """Схема для создания нового ограничения."""
 
     name: str
     description: str | None
@@ -11,13 +17,13 @@ class PermissionCreate(BaseModel):
 
 
 class PermissionRead(PermissionCreate):
-    """Схема для получения информации о существующей роли."""
+    """Схема для получения информации о существующем ограничении."""
 
     id: UUID
 
 
 class PermissionUpdate(BaseModel):
-    """Схема для обновления информации о существующей роли."""
+    """Схема для обновления информации о существующем ограничении."""
 
     description: str | None
     resource: str | None
